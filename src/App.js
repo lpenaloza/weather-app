0d7986af00ad703ff55e14ed2201c62d3627831a
DrojasFrontend <danielrojas243@gmail.com>
2020-01-28 12:03:52 -0500

    Iniciando Weather App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WeatherLocation from './components/WeatherLocation';
+import Weatherdata from './components/WeatherData';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherLocation/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Location.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Location = () => (
+  <div>
+    <h1>Barranquilla</h1>
+  </div>
+);
+
+export default Location;
